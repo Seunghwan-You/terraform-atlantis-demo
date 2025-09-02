eff5daecf1b92ae6255f2746c7d845f3eebe065c
yooshwan <yooshwan@mz.co.kr>
2025-09-02 15:00:00 +0900

    terraform_add

---
+++ environments/dev/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "<REPLACE_WITH_RG_NAME>"            # 예: rg-terraform-state
+    storage_account_name = "<REPLACE_WITH_STORAGE_ACCOUNT_NAME>" # 예: satfstate12345678
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+module "networking" {
+  source              = "../../modules/networking"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
+  vnet_cidr           = var.vnet_cidr
+  subnet_cidr         = var.subnet_cidr
+}
+
+module "vm" {
+  source              = "../../modules/vm"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = local.location
+  subnet_id           = module.networking.subnet_id
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+}
+
+output "public_ip" {
+  value = module.vm.public_ip
+}
+
+
---
+++ environments/dev/terraform.tfvars
@@ -0,0 +1,5 @@
+resource_group_name = "rg-atlantis-dev"
+location            = "koreacentral"
+admin_password      = "Pa$$w0rd123!" # 데모용. 실제로는 환경변수/키볼트/Atlantis var로 주입하세요.
+
+
