22dccc86f87890600b18535d0c6fa25127047705
yooshwan <yooshwan@mz.co.kr>
2025-09-02 15:01:48 +0900

    terraform_add2

---
+++ environments/prod/variables.tf
@@ -0,0 +1,36 @@
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "koreacentral"
+}
+
+variable "vnet_cidr" {
+  description = "CIDR for VNet"
+  type        = string
+  default     = "10.20.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR for Subnet"
+  type        = string
+  default     = "10.20.1.0/24"
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for VM login"
+  type        = string
+  sensitive   = true
+}
+
+
